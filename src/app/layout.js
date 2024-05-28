826e2ee137111a00d636255b0ac686fe479b0919
Wingscape <wingscape17@gmail.com>
2024-05-28 23:51:56 +0700

    Created navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Arimo } from "next/font/google";
+import Image from "next/image";
 import "./globals.css";
 
 const arimoFont = Arimo({ subsets: ["latin"] });
@@ -8,7 +9,7 @@ export const metadata = {
     description: "An independent IT service provider",
     authors: [{name: 'Naufal Fatihul Ihsan', url: 'https://github.com/Wingscape'}],
     icons: {
-        icon: '/favicon.ico'
+        icon: '/icon.ico'
     }
 };
 
@@ -16,8 +17,25 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={arimoFont.className}>
-                <header>
-                    <nav></nav>
+                <header className="bg-sky-500 px-[72px] py-6">
+                    <nav>
+                        <Image 
+                            src="/FalTiSan.svg"
+                            alt="Logo"
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            style={{
+                                width: 'auto',
+                                height: '24px'
+                            }}
+                        />
+                        <ul>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Portfolio</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </nav>
                 </header>
                 {children}
                 <footer></footer>
